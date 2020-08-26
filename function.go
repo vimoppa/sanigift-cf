f8556c330c1ae8db19a43412880560bb4305ec9a
Josh <mail@chukwuanu.com>
2020-08-26 16:47:49 +0100

    add cors headers

--- function.go
+++ function.go
@@ -37,6 +37,19 @@ func UploadImage(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(res{Message: "File Uploaded"})
 	}()
 
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	// Set CORS headers for the main request.
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*5)
 	defer cancel()
 
